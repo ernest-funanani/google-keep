5aa9891a2397c653f8117df35d90cadf7153c0ba
ernest funani <ernestmazuwo@gmail.com>
2022-03-09 10:36:50 +0200

    learning DOM

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ class Note {
 
 class App {
   constructor() {
-    this.notes = [new Note (1, "test title", "Test Text")];
+    this.notes = [new Note("abc1", "test title", "test text")];
 
     this.$activeForm = document.querySelector(".active-form");
     this.$inactiveForm = document.querySelector(".inactive-form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$notes = document.querySelector(".notes");
-    this.$form = document.querySelector("#form")
+    this.$form = document.querySelector("#form");
+    this.$modal = document.querySelector(".modal");
+    this.$modalForm = document.querySelector("#modal-form");
 
     this.addEventListeners();
     this.displayNotes();
@@ -24,13 +26,15 @@ class App {
   addEventListeners() {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
+      this.closeModal(event);
+      this.openModal(event);
     });
 
     this.$form.addEventListener("submit", (event) => {
-      event.preventDefaults();
+      event.preventDefault();
       const title = this.$noteTitle.value;
-      const text =  this.$noteText.value;
-      this.addNote({title, text});
+      const text = this.$noteText.value;
+      this.addNote({ title, text });
       this.closeActiveForm();
     });
   }
@@ -39,31 +43,42 @@ class App {
     const isActiveFormClickedOn = this.$activeForm.contains(event.target);
     const isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
     const title = this.$noteTitle.value;
-    const text =  this.$noteText.value;
+    const text = this.$noteText.value;
 
     if (isInactiveFormClickedOn) {
       this.openActiveForm();
-    }
-
-    else if(!isInactiveFormClickedOn && !isActiveFormClickedOn){
-      this.addNote({title, text});
+    } else if (!isInactiveFormClickedOn && !isActiveFormClickedOn) {
+      this.addNote({ title, text });
       this.closeActiveForm();
     }
   }
-  openActiveForm(){
+
+  openActiveForm() {
     this.$inactiveForm.style.display = "none";
     this.$activeForm.style.display = "block";
     this.$noteText.focus();
   }
-  closeActiveForm(){
+  closeActiveForm() {
     this.$inactiveForm.style.display = "block";
     this.$activeForm.style.display = "none";
+    this.$noteText.value = "";
     this.$noteTitle.value = "";
-    this.$noteText.value ="";
+  }
+
+  openModal(event) {
+    if (event.target.closest(".note")) {
+      this.$modal.classList.add("open-modal");
+    }
+  }
+  closeModal(event) {
+    const isModalFormClickedOn = this.$modalForm.contains(event.target);
+    if (!isModalFormClickedOn && this.$modal.classList.contains("open-modal")) {
+      this.$modal.classList.remove("open-modal");
+    }
   }
 
   addNote({ title, text }) {
-    if (text != ""){
+    if (text != "") {
       const newNote = new Note(cuid(), title, text);
       this.notes = [...this.notes, newNote];
       this.displayNotes();
@@ -84,54 +99,74 @@ class App {
     this.notes = this.notes.filter((note) => note.id != id);
   }
 
+  handleMouseOverNote(element) {
+    const $note = document.querySelector("#" + element.id);
+    const $checkNote = $note.querySelector(".check-circle");
+    const $noteFooter = $note.querySelector(".note-footer");
+    $checkNote.style.visibility = "visible";
+    $noteFooter.style.visibility = "visible";
+  }
+  handleMouseOutNote(element) {
+    const $note = document.querySelector("#" + element.id);
+    const $checkNote = $note.querySelector(".check-circle");
+    const $noteFooter = $note.querySelector(".note-footer");
+    $checkNote.style.visibility = "hidden";
+    $noteFooter.style.visibility = "hidden";
+  }
+
+  // onmouseover="app.handleMouseOverNote(this)" onmouseout="app.handleMouseOutNote(this)"
+
   displayNotes() {
-    this.$notes.innerHTML = this.notes.map((note) =>
-      `
-      <div class="note" id ="${note.id}" onclick="alert('hi')">
-      <span class="material-icons check-circle ">check_circle</span>
-      <div class="title ">${note.title}</div>
-      <div class="text ">${note.text}</div>
-      <div class="note-footer ">
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >add_alert</span
-          >
-          <span class="tooltip-text ">Remind me</span>
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) =>
+          `
+        <div class="note" id="${note.id}">
+          <span class="material-icons check-circle">check_circle</span>
+          <div class="title">${note.title}</div>
+          <div class="text">${note.text}</div>
+          <div class="note-footer">
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >add_alert</span
+              >
+              <span class="tooltip-text">Remind me</span>
+            </div>
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >person_add</span
+              >
+              <span class="tooltip-text">Collaborator</span>
+            </div>
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >palette</span
+              >
+              <span class="tooltip-text">Change Color</span>
+            </div>
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >image</span
+              >
+              <span class="tooltip-text">Add Image</span>
+            </div>
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >archive</span
+              >
+              <span class="tooltip-text">Archive</span>
+            </div>
+            <div class="tooltip">
+              <span class="material-icons-outlined hover small-icon"
+                >more_vert</span
+              >
+              <span class="tooltip-text">More</span>
+            </div>
+          </div>
         </div>
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >person_add</span
-          >
-          <span class="tooltip-text ">Collaborator</span>
-        </div>
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >palette</span
-          >
-          <span class="tooltip-text ">Change Color</span>
-        </div>
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >image</span
-          >
-          <span class="tooltip-text ">Add Image</span>
-        </div>
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >archive</span
-          >
-          <span class="tooltip-text ">Archive</span>
-        </div>
-        <div class="tooltip ">
-          <span class="material-icons-outlined hover small-icon "
-            >more_vert</span
-          >
-          <span class="tooltip-text ">More</span>
-        </div>
-      </div>
-    </div>
-      `
-    ).join("");
+    `
+      )
+      .join("");
   }
 }
 
